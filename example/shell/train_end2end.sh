99fea21b84cc4b1b6cb794c703d331a32394ad8a
maxtom <hitmaxtom@gmail.com>
2017-01-22 13:11:04 +0800

    ok

--- example/shell/train_end2end.sh
+++ example/shell/train_end2end.sh
@@ -5,5 +5,5 @@ python example/env/train_end2end.py \
 --begin_epoch 1 --end_epoch 20 \
 --lr 0.00001 --lr_step 30000 --gpus 6 --root_path 'data' \
 --dataset_path 'data/kitti' --dataset 'Kitti' --image_set 'val' \
---frequent 20 2>&1 | tee -a train_ry_alpha_car_only_reg.log
+--frequent 20 2>&1 | tee -a train_3dbox.log
 
