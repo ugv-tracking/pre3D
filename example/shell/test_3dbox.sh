06cf2343474ad060d61b9801347f434ee1c39349
maxtom <hitmaxtom@gmail.com>
2017-02-08 13:33:27 +0800

    problem with rotation

--- example/shell/test_3dbox.sh
+++ example/shell/test_3dbox.sh
@@ -1,2 +1,2 @@
 # Origional Faster RCNN test
-python example/env/test.py --prefix model/3dbox/3dbox --epoch 6 --image data/kitti/test/000046.png --gpu 6 --bbox
+python example/env/test.py --prefix model/3dbox/3dbox --epoch 6 --image data/kitti/test/000098.png --gpu 6 --bbox
--- example/shell/train_3dbox.sh
+++ example/shell/train_3dbox.sh
@@ -1,6 +1,6 @@
 python example/env/train_end2end.py \
---pretrained model/3dbox/3dbox --epoch 1 \
---prefix model/3dbox/3dbox --begin_epoch 1 --end_epoch 500 \
+--pretrained model/3dbox/3dbox --epoch 6 \
+--prefix model/3dbox/3dbox --begin_epoch 6 --end_epoch 500 \
 --lr 0.00001 --lr_step 30000 --gpus 6 --root_path 'data' \
 --dataset_path 'data/kitti' --dataset 'Kitti' --image_set 'val' \
 --bbox \
